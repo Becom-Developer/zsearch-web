de4105691cd39a8c4ff234ac088348643967cec9
yk <yosizuka1@gmail.com>
2022-05-09 15:48:28 +0900

    gtm

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-5SH57MX"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe
+    ></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <b-navbar toggleable="lg" type="dark" variant="info" class="sticky-top">
       <b-navbar-brand style="cursor: pointer" @click="$router.push('/')"
         >zsearch - 住所検索</b-navbar-brand
--- nuxt.config.js
+++ nuxt.config.js
@@ -40,16 +40,13 @@ export default {
     ],
     script: [
       {
-        async: true,
-        src: 'https://www.googletagmanager.com/gtag/js?id=G-G63CW0K09H',
-      },
-      {
+        hid: 'gtmHead',
         innerHTML: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-G63CW0K09H');
-        `,
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5SH57MX');`,
       },
     ],
   },
